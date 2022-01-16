0dac6c280a8dcf05a9192bb7d9051c11342ff8f0
Dustin Ray <dustinvonsandwich@gmail.com>
2022-01-16 13:22:42 -0800

    abstract player types to player object

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
+//main class for game, will initiate game assets
+
+//init a fresh game engine and asset manager
 const gameEngine = new GameEngine();
 const ASSET_MANAGER = new AssetManager();
+
+//although these assets are technically loaded in the entity classes,
+// they must also be downloaded by the asset mangager here as well. (afik)
 ASSET_MANAGER.queueDownload("./sprites/sprite_sheet.png");
 ASSET_MANAGER.queueDownload("./sprites/washing_machine/opening/washing_machine_sprite_sheet.png");
 ASSET_MANAGER.queueDownload("./sprites/washing_machine/walking/washing_machine_walking_sprite_sheet.png");
 ASSET_MANAGER.queueDownload("./sprites/jump/jump_sprite_sheet.png");
 ASSET_MANAGER.queueDownload("./sprites/duckandslide/duckandslide.png");
 
+//trigger downloads and add an entity to the canvas
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
 
-	//gameEngine.addEntity(new WashingMachine(gameEngine));
-	gameEngine.addEntity(new Test_sprite(gameEngine));
+	// add a player entity with keyword "washing_machine" 
+	// to indicate player type
+	gameEngine.addEntity(new Player(gameEngine, "washing_machine"));
 	gameEngine.init(ctx);
 	gameEngine.start();
 
