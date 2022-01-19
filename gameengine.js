673927f3ca15bf440eafdff19222c41c3e1af83d
algorithm0r <algorithm0r@gmail.com>
2022-01-19 15:27:44 -0800

    Reverted some more

--- gameengine.js
+++ gameengine.js
@@ -15,15 +15,8 @@ class GameEngine {
         this.wheel = null;
         this.keys = {};
 
-        // THE KILL SWITCH
-        this.running = false;
-
         // Options and the Details
         this.options = options || {
-            prevent: {
-                contextMenu: true,
-                scrolling: true,
-            },
             debugging: false,
         };
     };
@@ -38,9 +31,7 @@ class GameEngine {
         this.running = true;
         const gameLoop = () => {
             this.loop();
-            if (this.running) {
-                requestAnimFrame(gameLoop, this.ctx.canvas);
-            }
+            requestAnimFrame(gameLoop, this.ctx.canvas);
         };
         gameLoop();
     };
@@ -69,9 +60,7 @@ class GameEngine {
             if (this.options.debugging) {
                 console.log("WHEEL", getXandY(e), e.wheelDelta);
             }
-            if (this.options.prevent.scrolling) {
-                e.preventDefault(); // Prevent Scrolling
-            }
+            e.preventDefault(); // Prevent Scrolling
             this.wheel = e;
         });
 
@@ -79,14 +68,12 @@ class GameEngine {
             if (this.options.debugging) {
                 console.log("RIGHT_CLICK", getXandY(e));
             }
-            if (this.options.prevent.contextMenu) {
-                e.preventDefault(); // Prevent Context Menu
-            }
+            e.preventDefault(); // Prevent Context Menu
             this.rightclick = getXandY(e);
         });
 
-        window.addEventListener("keydown", event => this.keys[event.key] = true);
-        window.addEventListener("keyup", event => this.keys[event.key] = false);
+        this.ctx.canvas.addEventListener("keydown", event => this.keys[event.key] = true);
+        this.ctx.canvas.addEventListener("keyup", event => this.keys[event.key] = false);
     };
 
     addEntity(entity) {
