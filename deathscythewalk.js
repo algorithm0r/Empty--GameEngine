62bebf0269a7031289a4d669f29ff81fb2be17f3
Michael <michaeltheisen@hotmail.com>
2023-01-08 05:01:19 -0800

    moved sprite to folder, changed paths

--- deathscythewalk.js
+++ deathscythewalk.js
@@ -1,7 +1,7 @@
 class Deathscythewalk {
     constructor(game) {
         this.game = game;   
-        this.animator = new Animator(ASSET_MANAGER.getAsset("./deathscythewalk.png"), 
+        this.animator = new Animator(ASSET_MANAGER.getAsset("./Sprites/deathscythewalk.png"), 
         0, 0, 80, 114, 12, 0.2);
         // the width of the deathscyth in pixels is 80 pixels 
         // the height of the deathscyth in pixels is 114 pixels
--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
 
-ASSET_MANAGER.queueDownload("./deathscythewalk.png");
+ASSET_MANAGER.queueDownload("./Sprites/deathscythewalk.png");
 
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
