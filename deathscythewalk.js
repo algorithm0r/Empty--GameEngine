7bc2d533dad7a2302ff68948a3f5ece46941afa8
Michael <michaeltheisen@hotmail.com>
2023-01-08 03:57:41 -0800

    Attempt 2 after bug fix

--- deathscythewalk.js
+++ deathscythewalk.js
@@ -1,5 +1,6 @@
 class Deathscythewalk {
-    constructor() {
+    constructor(game) {
+        this.game = game;   
         this.animator = new Animator(ASSET_MANAGER.getAsset("./deathscythewalk.png"), 
         0, 0, 80, 114, 12, 0.2);
         // the width of the deathscyth in pixels is 80 pixels 
@@ -12,7 +13,6 @@ class Deathscythewalk {
     };
 
     draw(ctx){
-        ctx.drawImage(ASSET_MANAGER.getAsset("./deathscythewalk.png"), 0, 0);
-
+        this.animator.drawFrame(this.game.clockTick, ctx, 25, 25);
     };
 }
--- main.js
+++ main.js
@@ -8,7 +8,7 @@ ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
 
-	gameEngine.addEntity(new Deathscythewalk());
+	gameEngine.addEntity(new Deathscythewalk(gameEngine));
 
 	gameEngine.init(ctx);
 
