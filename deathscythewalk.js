f4ee94735e17166bd5c553b0ed3283bca78270d3
Michael Theisen <60194610+MJTheisen@users.noreply.github.com>
2023-01-08 03:19:31 -0800

    Add files via upload

---
+++ deathscythewalk.js
@@ -0,0 +1,14 @@
+class Deathscythewalk {
+    constructor() {
+
+    };
+
+    update() {
+
+    };
+
+    draw(ctx){
+        ctx.drawImage(ASSET_MANAGER.getAsset("./deathscythewalk.png"),0,0);
+
+    };
+}
--- main.js
+++ main.js
@@ -2,9 +2,13 @@ const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
 
+ASSET_MANAGER.queueDownload("./deathscythewalk.png");
+
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
+
+	gameEngine.addEntity(new Deathscythewalk());
 
 	gameEngine.init(ctx);
 
