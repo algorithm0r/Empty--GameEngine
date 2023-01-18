e943abdb86678368c307d41fbd017e332b5ce751
Deep <115367789+deepuh@users.noreply.github.com>
2023-01-18 12:11:02 -0800

    Add files via upload

---
+++ Animator.js
@@ -0,0 +1,30 @@
+class Animator {
+    constructor(spritesheet, xStart, yStart, width, height, frameCount, frameDuration) {
+        Object.assign(this, { spritesheet, xStart, yStart, width, height, frameCount, frameDuration });
+
+        this.elapsedTime = 0;
+        this.totalTime = frameCount * frameDuration;
+    };
+
+    drawFrame(tick, ctx, x, y) {
+        this.elapsedTime += tick;
+
+        if (this.elapsedTime > this.totalTime) this.elapsedTime -= this.totalTime;
+
+        const frame = this.currentFrame();
+
+        ctx.drawImage(this.spritesheet,
+            this.xStart + this.width * frame, this.yStart,
+            this.width, this.height,
+            x, y,
+            this.width * 2, this.height * 2);
+    };
+
+    currentFrame() {
+        return Math.floor(this.elapsedTime / this.frameDuration);
+    };
+
+    isDone() {
+        return (this.elapsedTime >= this.totalTime);
+    };
+};
