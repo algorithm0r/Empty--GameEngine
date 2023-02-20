9bb467701087288b8c68f282437d9753bfec7474
jaythree <jaythree206@gmail.com>
2023-02-19 16:19:10 -0800

    Download all chaneg to fix music

--- assetmanager.js
+++ assetmanager.js
@@ -17,32 +17,37 @@ class AssetManager {
 
     downloadAll(callback) {
         if (this.downloadQueue.length === 0) setTimeout(callback, 10);
-        for (let i = 0; i < this.downloadQueue.length; i++) {
+        for (var i = 0; i < this.downloadQueue.length; i++) {
+            var that = this;
 
-            const path = this.downloadQueue[i];
+            var path = this.downloadQueue[i];
             console.log(path);
             var ext = path.substring(path.length - 3);
+
             switch (ext) {
+                case 'jpg':
                 case 'png':
-                    const img = new Image();
-                    img.addEventListener("load", () => {
-                        console.log("Loaded " + img.src);
-                        this.successCount++;
-                        if (this.isDone()) callback();
+                    var img = new Image();
+                    img.addEventListener("load", function () {
+                        console.log("Loaded " + this.src);
+                        that.successCount++;
+                        if (that.isDone()) callback();
                     });
 
-                    img.addEventListener("error", () => {
-                        console.log("Error loading " + img.src);
-                        this.errorCount++;
-                        if (this.isDone()) callback();
+                    img.addEventListener("error", function () {
+                        console.log("Error loading " + this.src);
+                        that.errorCount++;
+                        if (that.isDone()) callback();
                     });
 
                     img.src = path;
                     this.cache[path] = img;
                     break;
+                case 'wav':
                 case 'mp3':
+                case 'mp4':
                     var aud = new Audio();
-                    aud.addEventListener("loadeddata", function() {
+                    aud.addEventListener("loadeddata", function () {
                         console.log("Loaded " + this.src);
                         that.successCount++;
                         if (that.isDone()) callback();
@@ -62,10 +67,9 @@ class AssetManager {
                     aud.src = path;
                     aud.load();
 
-                    this.cache[path] = new aud;
+                    this.cache[path] = aud;
                     break;
             }
-            
         }
     };
 
@@ -73,10 +77,20 @@ class AssetManager {
         return this.cache[path];
     };
 
-     playAsset(path) {
+    playAsset(path) {
         let audio = this.cache[path];
         audio.currentTime = 0;
         audio.play();
+    };
+
+    pauseBackgroundMusic() {
+        for (var key in this.cache) {
+            let asset = this.cache[key];
+            if (asset instanceof Audio) {
+                asset.pause();
+                asset.currentTime = 0;
+            }
+        }
     };
 
     autoRepeat(path) {
