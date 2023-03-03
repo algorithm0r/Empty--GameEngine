7259785a119f38a512a2ae7ce8c03595d183c1e8
Deep Singh <deepsingh.khalsa25@gmail.com>
2023-03-03 13:35:25 -0800

    made spawning further

--- spawnEnemies.js
+++ spawnEnemies.js
@@ -13,13 +13,17 @@ class SpawnMobs {
     };
 
     spawnEnemies() {
+        this.xrandomMax = this.game.camera.ship.x + 750;
+        this.xrandomMin = this.game.camera.ship.x - 750;
+        this.yrandomMax = this.game.camera.ship.y + 750;
+        this.yrandomMin = this.game.camera.ship.y - 750;
 
         this.game.camera.spawntimer += this.game.clockTick;
         this.game.camera.slimetimer += this.game.clockTick;
         if(this.game.camera.time) { //keep spawning slimes
             if(this.game.camera.slimetimer >= 1) {
-                this.spawnx = getRandomInt(this.game.camera.ship.x + 500, this.game.camera.ship.x - 500);
-                this.spawny = getRandomInt(this.game.camera.ship.y + 500, this.game.camera.ship.y - 500);
+                this.spawnx = getRandomInt(this.xrandomMax, this.xrandomMin);
+                this.spawny = getRandomInt(this.yrandomMax, this.yrandomMin);
                 this.game.addEntity(new Slime(this.game, this.spawnx, this.spawny, this.game.player));
                 this.game.camera.slimetimer = 0;
             }
@@ -29,13 +33,13 @@ class SpawnMobs {
                 this.game.camera.spawnchance = getRandomInt(0,100)
                 console.log("Monster1 spawn chance = " + this.game.camera.spawnchance);
                 if(this.game.camera.spawnchance >= 50) {
-                    this.spawnx = getRandomInt(this.game.camera.ship.x + 500, this.game.camera.ship.x - 500);
-                    this.spawny = getRandomInt(this.game.camera.ship.y + 500, this.game.camera.ship.y - 500);
+                    this.spawnx = getRandomInt(this.xrandomMax, this.xrandomMin);
+                    this.spawny = getRandomInt(this.yrandomMax, this.yrandomMin);
                     this.game.addEntity(new Monster1(this.game, this.spawnx, this.spawny, this.game.player));
                 } 
                 if(this.game.camera.spawnchance >= 98) {
-                    this.spawnx = getRandomInt(this.game.camera.ship.x + 500, this.game.camera.ship.x - 500);
-                    this.spawny = getRandomInt(this.game.camera.ship.y + 500, this.game.camera.ship.y - 500);
+                    this.spawnx = getRandomInt(this.xrandomMax, this.xrandomMin);
+                    this.spawny = getRandomInt(this.yrandomMax, this.yrandomMin);
                     this.game.addEntity(new EnemyShip(this.game, this.spawnx, this.spawny, this.game.player));
                 }
                 this.game.camera.spawntimer = 0;
@@ -44,13 +48,13 @@ class SpawnMobs {
                 this.game.camera.spawnchance = getRandomInt(0,100)
                 console.log("Monster1 spawn chance = " + this.game.camera.spawnchance);
                 if(this.game.camera.spawnchance >= 40) {
-                    this.spawnx = getRandomInt(this.game.camera.ship.x + 500, this.game.camera.ship.x - 500);
-                    this.spawny = getRandomInt(this.game.camera.ship.y + 500, this.game.camera.ship.y - 500);
+                    this.spawnx = getRandomInt(this.xrandomMax, this.xrandomMin);
+                    this.spawny = getRandomInt(this.yrandomMax, this.yrandomMin);
                     this.game.addEntity(new Monster1(this.game, this.spawnx, this.spawny, this.game.player));
                 } 
                 if(this.game.camera.spawnchance >= 93) {
-                    this.spawnx = getRandomInt(this.game.camera.ship.x + 500, this.game.camera.ship.x - 500);
-                    this.spawny = getRandomInt(this.game.camera.ship.y + 500, this.game.camera.ship.y - 500);
+                    this.spawnx = getRandomInt(this.xrandomMax, this.xrandomMin);
+                    this.spawny = getRandomInt(this.yrandomMax, this.yrandomMin);
                     this.game.addEntity(new EnemyShip(this.game, this.spawnx, this.spawny, this.game.player));
                 }
                 this.game.camera.spawntimer = 0;
@@ -59,13 +63,13 @@ class SpawnMobs {
                 this.game.camera.spawnchance = getRandomInt(0,100)
                 console.log("Monster1 spawn chance = " + this.spawnchance);
                 if(this.game.camera.spawnchance >= 30) {
-                    this.spawnx = getRandomInt(this.game.camera.ship.x + 500, this.game.camera.ship.x - 500);
-                    this.spawny = getRandomInt(this.game.camera.ship.y + 500, this.game.camera.ship.y - 500);
+                    this.spawnx = getRandomInt(this.xrandomMax, this.xrandomMin);
+                    this.spawny = getRandomInt(this.yrandomMax, this.yrandomMin);
                     this.game.addEntity(new Monster1(this.game, this.spawnx, this.spawny, this.game.player));
                 } 
                 if(this.game.camera.spawnchance >= 90) {
-                    this.spawnx = getRandomInt(this.game.camera.ship.x + 500, this.game.camera.ship.x - 500);
-                    this.spawny = getRandomInt(this.game.camera.ship.y + 500, this.game.camera.ship.y - 500);
+                    this.spawnx = getRandomInt(this.xrandomMax, this.xrandomMin);
+                    this.spawny = getRandomInt(this.yrandomMax, this.yrandomMin);
                     this.game.addEntity(new EnemyShip(this.game, this.spawnx, this.spawny, this.game.player));
                 }
                 this.game.camera.spawntimer = 0;
@@ -74,14 +78,14 @@ class SpawnMobs {
                 this.game.camera.spawnchance = getRandomInt(0,100)
                 console.log("Monster1 spawn chance = " + this.spawnchance);
                 if(this.game.camera.spawnchance >= 20) {
-                    this.spawnx = getRandomInt(this.game.camera.ship.x + 500, this.game.camera.ship.x - 500);
-                    this.spawny = getRandomInt(this.game.camera.ship.y + 500, this.game.camera.ship.y - 500);
+                    this.spawnx = getRandomInt(this.xrandomMax, this.xrandomMin);
+                    this.spawny = getRandomInt(this.yrandomMax, this.yrandomMin);
                     this.game.addEntity(new Monster1(this.game, this.spawnx, this.spawny, this.game.player));
                     this.game.addEntity(new Slime(this.game, this.spawnx, this.spawny, this.game.player));
                 } 
                 if(this.game.camera.spawnchance >= 85) {
-                    this.spawnx = getRandomInt(this.game.camera.ship.x + 500, this.game.camera.ship.x - 500);
-                    this.spawny = getRandomInt(this.game.camera.ship.y + 500, this.game.camera.ship.y - 500);
+                    this.spawnx = getRandomInt(this.xrandomMax, this.xrandomMin);
+                    this.spawny = getRandomInt(this.yrandomMax, this.yrandomMin);
                     this.game.addEntity(new EnemyShip(this.game, this.spawnx, this.spawny, this.game.player));
                 }
                 this.game.camera.spawntimer = 0;
